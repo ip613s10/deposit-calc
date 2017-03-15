39fc9cb9d965ac139e8e19f95977d3d463710b2e
Земцов Тимофей <timzimo@mail.ru>
2017-03-16 02:29:41 +0700

    Redesigned calculations

--- calc2.cpp
+++ calc2.cpp
@@ -12,6 +12,32 @@ int Check (int d, int p) {
     }
     return 0;
 };
+
+void Profit (int d,int p) {
+    if (d > 100000) {
+        if (p <= 30)
+            d = d - d / 10;
+    if (p > 30 && p <= 120) 
+        d = 1.03 * d;
+    if (p > 120 && p <= 240)
+        d = 1.08 * d;
+    if (p > 240 && p <= 365)
+        d = 1.15 * d;
+    }
+
+    if (d <= 100000) {
+        if (p <= 30)
+            d = d - d / 10;
+    if (p > 30 && p <= 120) 
+        d = 1.02 * d;
+    if (p > 120 && p <= 240)
+        d = 1.06 * d;
+    if (p > 240 && p <= 365)
+        d = 1.12 * d;
+    }
+
+printf ("\nProfit=%d\n", d);   
+};
     
 int main() {
 int d, p;  // d - deposit, p - period
@@ -24,29 +50,8 @@ do {
 
 printf ("d:%d p:%d\n", d, p);
   
-if (d > 100000) {
-    if (p <= 30)
-        d = d - d / 10;
-    if (p > 30 && p <= 120) 
-        d = 1.03 * d;
-    if (p > 120 && p <= 240)
-        d = 1.08 * d;
-    if (p > 240 && p <= 365)
-        d = 1.15 * d;
-}
+Profit(d, p);
 
-    if (d <= 100000) {
-        if (p <= 30)
-            d = d - d / 10;
-    if (p > 30 && p <= 120) 
-            d = 1.02 * d;
-    if (p > 120 && p <= 240)
-            d = 1.06 * d;
-    if (p > 240 && p <= 365)
-            d = 1.12 * d;
-}
-
-printf ("\nprofit=%d\n", d);
 system("pause");
 return 0;
 }
