3b3388f0ca90025a656a0a5743eef43c9bc5c4a6
Timofei Zemtcov <timzimo@mail.ru>
2017-05-24 22:09:16 +0700

    Changes and additions in tests

--- test/deposit-test.c
+++ test/deposit-test.c
@@ -1,27 +1,35 @@
 #include <ctest.h>
 #include <../src/deposit.h>
 
-CTEST(input_check, test_input)
+CTEST(input_check_more_than_1_year, test_input)
 {
     int result = Check(777777, 555);
-    
     int expected = 0;
+    
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(input_check, test_input)
 {
-    int result = Check(1000, 100);
-    
+    int result = Check(1000, 100);  
     int expected = 1;
-    ASSERT_EQUAL(expected, result);
+    
+    ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_test, calc)
 {
-    int result = Profit(123456, 150);
+    double result = Profit(123456, 150);
+    const double exp = 133332;
     
-    const int expected = 133332;
+    ASSERT_DBL_NEAR(exp, result);
+}
+
+CTEST(calc_test_less_than_0_days, calc)
+{
+    double result = Profit(123456, -10);
+    const double exp = 0;
+    
     ASSERT_DBL_NEAR(exp, result);
 }
 
@@ -29,16 +37,16 @@ CTEST(calc_test, calc)
 
 CTEST(calc_30_day_less_than_100k, calc)
 {
-    int result = Profit(50000, 30);
-    const int exp = 45000;
+    double result = Profit(50000, 30);
+    const double exp = 45000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_30_day_more_than_100k, calc) 
 {
-    int result = Profit(150000, 30);
-    const int exp = 135000;
+    double result = Profit(150000, 30);
+    const double exp = 135000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
@@ -47,16 +55,16 @@ CTEST(calc_30_day_more_than_100k, calc)
 
 CTEST(calc_31_day_less_than_100k, calc)
 {
-    int result = Profit(50000, 31);
-    const int exp = 51000;
+    double result = Profit(50000, 31);
+    const double exp = 51000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_31_day_more_than_100k, calc)
 {
-    int result = Profit(150000, 31);
-    const int exp = 154500;
+    double result = Profit(150000, 31);
+    const double exp = 154500;
 
     ASSERT_DBL_NEAR(exp, result);
 }
@@ -64,16 +72,16 @@ CTEST(calc_31_day_more_than_100k, calc)
 
 CTEST(calc_120_day_less_100000_rub, calc)
 {
-    int result = Profit(50000, 31);
-    const int exp = 51000;
+    double result = Profit(50000, 31);
+    const double exp = 51000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_120_day_more_100000_rub, calc)
 {
-    int result = Profit(150000, 31);
-    const int exp = 154500;
+    double result = Profit(150000, 31);
+    const double exp = 154500;
 
     ASSERT_DBL_NEAR(exp, result);
 }
@@ -81,16 +89,16 @@ CTEST(calc_120_day_more_100000_rub, calc)
 
 CTEST(calc_121_day_less_than_100k, calc)
 {
-    int result = Profit(50000, 121);
-    const int exp = 53000;
+    double result = Profit(50000, 121);
+    const double exp = 53000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_121_day_more_than_100k, calc)
 {
-    int result = Profit(150000, 121);
-    const int exp = 162000;
+    double result = Profit(150000, 121);
+    const double exp = 162000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
@@ -99,16 +107,16 @@ CTEST(calc_121_day_more_than_100k, calc)
 
 CTEST(calc_240_day_less_than_100k, calc)
 {
-    int result = Profit(50000, 121);
-    const int exp = 53000;
+    double result = Profit(50000, 121);
+    const double exp = 53000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_240_day_more_than_100k, calc)
 {
-    int result = Profit(150000, 121);
-    const int exp = 162000;
+    double result = Profit(150000, 121);
+    const double exp = 162000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
@@ -117,16 +125,16 @@ CTEST(calc_240_day_more_than_100k, calc)
 
 CTEST(calc_241_day_less_than_100k, calc)
 {
-    int result = Profit(50000, 241);
-    const int exp = 56000;
+    double result = Profit(50000, 241);
+    const double exp = 56000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_241_day_more_than_100k, calc)
 {
-    int result = Profit(150000, 241);
-    const int exp = 172500;
+    double result = Profit(150000, 241);
+    const double exp = 172500;
 
     ASSERT_DBL_NEAR(exp, result);
 }
@@ -134,16 +142,16 @@ CTEST(calc_241_day_more_than_100k, calc)
 
 CTEST(calc_365_day_less_than_100k, calc)
 {
-    int result = Profit(50000, 241);
-    const int exp = 56000;
+    double result = Profit(50000, 241);
+    const double exp = 56000;
 
     ASSERT_DBL_NEAR(exp, result);
 }
 
 CTEST(calc_365_day_more_than_100k, calc)
 {
-    int result = Profit(150000, 241);
-    const int exp = 172500;
+    double result = Profit(150000, 241);
+    const double exp = 172500;
 
     ASSERT_DBL_NEAR(exp, result);
 }
